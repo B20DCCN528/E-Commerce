be6d51532ee9fcbec374c233be444a42106c4dca
quang1105 <lazymoon2k2@gmail.com>
2023-08-04 01:01:39 +0700

    #2 Handler error

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
 
 require('dotenv').config()
 
@@ -6,6 +8,8 @@ const app = express()
 const port = process.env.port || 8888
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
+dbConnect()
+initRoutes(app)
 
 app.use('/', (req, res) => (res.send('SERVER ONN')))
 
